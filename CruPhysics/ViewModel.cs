31c6ae73ae424f5b88f9ac528a82d1c53a335b64
crupest <crupest@outlook.com>
2017-06-07 22:26:15 +0800

    Reorganize.

--- CruPhysics/ViewModel.cs
+++ CruPhysics/ViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 
+using CruPhysics.Windows;
 
 namespace CruPhysics
 {
--- CruPhysics/Windows/MainWindow.xaml.cs
+++ CruPhysics/Windows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CruPhysics
+namespace CruPhysics.Windows
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
