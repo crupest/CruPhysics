56e31985748cdb80e6e7e53868fe5ef177904e5e
crupest <crupest@outlook.com>
2017-07-03 22:35:06 +0800

    Delete some useless code.

--- CruPhysics/Common.cs
+++ CruPhysics/Common.cs
@@ -35,40 +35,6 @@ namespace CruPhysics
 
     public static class Common
     {
-        public delegate bool CheckValue(double value);
-
-        public static double ParseTextBox(TextBox textBox, ref string info)
-        {
-            return ParseTextBox(textBox, null, ref info);
-        }
-
-        public static double ParseTextBox(TextBox textBox, CheckValue checkValue, ref string info)
-        {
-            if (textBox == null)
-                throw new ArgumentNullException("textBox");
-            if (!(textBox.Tag is string))
-                throw new Exception("The Tag of textbox is not a string.");
-
-            double result = 0.0;
-            try
-            {
-                result = double.Parse(textBox.Text);
-                if (checkValue != null && !checkValue(result))
-                    throw new OverflowException();
-            }
-            catch (FormatException)
-            {
-                textBox.Background = Brushes.Red;
-                info += (string)textBox.Tag + "不是一个数字！\n";
-            }
-            catch (OverflowException)
-            {
-                textBox.Background = Brushes.Red;
-                info += (string)textBox.Tag + "超出范围！\n";
-            }
-            return result;
-        }
-
         /// <summary>
         /// 顺时针旋转一个向量一定角度
         /// </summary>
