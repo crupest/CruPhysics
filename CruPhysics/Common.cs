be7498a617eb3cc1dad008b62dc9724779076b4e
crupest <crupest@outlook.com>
2017-07-12 18:31:01 +0800

    Add PropertyManager, property-bind and unit test.

--- CruPhysics/Common.cs
+++ CruPhysics/Common.cs
@@ -41,6 +41,59 @@ namespace CruPhysics
         }
     }
 
+    public static class PropertyManager
+    {
+        public static object GetPropertyValue(object target, string propertyName)
+        {
+            return target.GetType().GetProperty(propertyName).GetValue(target);
+        }
+
+        public static void SetPropertyValue(object target, string propertyName, object value)
+        {
+            target.GetType().GetProperty(propertyName).SetValue(target, value);
+        }
+
+        public static void OneWayBind(INotifyPropertyChanged source, string sourcePropertyName, object target, string targetPropertyName)
+        {
+            source.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == sourcePropertyName)
+                {
+                    SetPropertyValue(target, targetPropertyName, GetPropertyValue(source, sourcePropertyName));
+                }
+            };
+        }
+
+        public static void TwoWayBind(INotifyPropertyChanged source, string sourcePropertyName, INotifyPropertyChanged target, string targetPropertyName)
+        {
+            bool b = true;
+            source.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == sourcePropertyName && b)
+                {
+                    b = !b;
+                    SetPropertyValue(target, targetPropertyName, GetPropertyValue(source, sourcePropertyName));
+                }
+                else
+                {
+                    b = !b;
+                }
+            };
+            target.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == targetPropertyName && b)
+                {
+                    b = !b;
+                    SetPropertyValue(source, sourcePropertyName, GetPropertyValue(target, targetPropertyName));
+                }
+                else
+                {
+                    b = !b;
+                }
+            };
+        }
+    }
+
     public static class Common
     {
         /// <summary>
