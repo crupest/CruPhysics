584fe826a4944942f8aeeb3e61572fcb9fdcf8e3
crupest <crupest@outlook.com>
2017-07-13 23:19:32 +0800

    ...

--- CruPhysics/Scene.cs
+++ CruPhysics/Scene.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using CruPhysics.Controls;
+using CruPhysics.Windows;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CruPhysics
 {
